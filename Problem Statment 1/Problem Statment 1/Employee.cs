621b4b0d507930c6bb0714e4b10d5d0e834aedd6
Satyam Bhalerao <65886879+satyambhalerao@users.noreply.github.com>
2023-04-22 18:09:21 +0530

    Persistent problem  1

---
+++ Problem Statment 1/Problem Statment 1/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Statment_1
+{
+		 class Employee : IComparable<Employee>
+	{
+		string name;
+		int salary;
+
+		public string Name
+		{
+			get { return name; } set {  name = value; }
+		}
+
+		public int Salray
+		{
+			get { return salary; } set {salary=value; }
+		}
+		public Employee() { }
+
+		public Employee(string name,int salary) 
+		{
+			Name = name;
+			Salray = salary;
+		}
+
+		public int CompareTo(Employee other)
+		{
+			if(Salray.CompareTo(other.Salray) != 0)
+			{
+				return Salray.CompareTo(other.Salray);
+			}
+			else
+			{
+				return Name.CompareTo(other.Name);
+			}
+		}
+
+		public override string ToString() 
+		{
+			return this.Name +" - "+this.Salray;
+		}
+	}
+}
---
+++ Problem Statment 1/Problem Statment 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Problem_Statment_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<Employee> employees = new List<Employee>();
+
+			Console.WriteLine("Enter Details of 5 empyoee");
+			for(int i = 0; i < 5; i++) 
+			{
+				Console.WriteLine("Enter your name");
+				string name = Console.ReadLine();
+
+				while(string.IsNullOrWhiteSpace(name))
+				{
+					Console.WriteLine("Invalid name: Enter your name again");
+					name = Console.ReadLine();
+				}
+				Console.WriteLine("Enter Salary");
+				int salary = int.Parse(Console.ReadLine());
+				
+				while(salary <= 0)
+				{
+					Console.WriteLine("invaild Salary : Enter your salary again");
+					salary = int.Parse(Console.ReadLine());
+				}
+
+				employees.Add(new Employee ( name, salary ));
+			}
+			employees.Sort();
+			foreach(Employee employee in employees)
+			{
+				Console.WriteLine(employee);
+			}
+		}
+	}
+}
